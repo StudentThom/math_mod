d1ea51af83bc27c9803e8a2530055f94f53000ae
thom <thom@laptop.com>
2018-01-11 22:31:37 +0100

    small changes

--- r_project/project1/run_em.R
+++ r_project/project1/run_em.R
@@ -4,11 +4,11 @@
 ### EXAMPLE 1 ###
 # variables
 data = allp$p1
-alpha = c(1,1,5)
-beta = c(1,3,1)
+alpha = c(1,1)
+beta = c(1,3)
 number_of_iterations = 25
 # initial value
-pi_vector = c(0.3,0.4,0.3)
+pi_vector = c(0.3,0.7)
 
 # run EM algo
 pi_vector <- em_algo(data,pi_vector,alpha,beta,number_of_iterations)
