c4832c6706a7b4cc36d3dd6269b6c634f93cd7a1
StudentThom <StudentThom@users.noreply.github.com>
2018-01-11 13:57:46 +0100

    plaatjes

---
+++ r_project/project1/run_em.R
@@ -0,0 +1,32 @@
+# run em algo en plot bby c alling functions
+
+
+### EXAMPLE 1 ###
+# variables
+data = allp$p1
+alpha = c(1,1,5)
+beta = c(1,3,1)
+number_of_iterations = 25
+# initial value
+pi_vector = c(0.3,0.4,0.3)
+
+# run EM algo
+pi_vector <- em_algo(data,pi_vector,alpha,beta,number_of_iterations)
+
+# plot result
+plot_distribution(data, pi_vector, alpha, beta)
+
+### EXAMPLE 2 ###
+# variables
+data = allp$p4
+alpha = c(1,1,5,1.1)
+beta = c(1,3,1,1)
+number_of_iterations = 15
+# initial value
+pi_vector = c(0.25,0.25,0.25,0.25)
+
+# run EM algo
+pi_vector <- em_algo(data,pi_vector,alpha,beta,number_of_iterations)
+
+# plot result
+plot_distribution(data, pi_vector, alpha, beta)
