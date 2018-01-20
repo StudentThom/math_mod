7e0feedcf6d5cd0d42738c0e2b2156c63c19ff22
thom <thom@laptop.com>
2018-01-20 16:07:35 +0100

    optim lijkt te gaan werken

--- r_project/project1/test_optim.R
+++ r_project/project1/test_optim.R
@@ -25,11 +25,16 @@ expectation_f <- function(alpha_beta,data, pi_vector, j, alphas, betas){
   alpha = alpha_beta[1]
   beta = alpha_beta[2]
   sum = 0
+  exp_resp <- 0
   for (i in 1:length(data)) {
-    sum = sum + (log(dbeta(data[i],alpha,beta)) * expected_resp(data[i], pi_vector, j, alphas, betas))
+    exp_resp <- (expected_resp(data[i], pi_vector, j, alphas, betas))
+    sum = sum + (log(dbeta(data[i],alpha,beta)) * exp_resp)
   }
   # return
   sum
 }
+
+# best
+out <- optim(c(1,4),expectation_f,lower=c(0.1,0.1),method="L-BFGS-B",data=allp$p1,pi_vector=c(0.2,0.8),j=1, alphas=c(1,1),betas=c(1,4),control=list(fnscale = -1))
 
 optim(par=c(1,4),expectation_f,data=allp$p1,pi_vector=c(0.3,0.7),j=2,alphas=c(1,1),betas=c(1,4),lower=c(0.1,0.1),method="L-BFGS-B")
